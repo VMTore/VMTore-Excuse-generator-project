18788c16096c2b8e1a01e10cf928eb7c4a93e698
Veronica <veronicamt85@gmail.com>
2024-04-26 20:44:28 +0200

    Versión final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//Arrays
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ['the', 'our', 'your',];
+let adj = ['great', 'big', 'fantastic', 'amazing'];
+let noun = ['jogger', 'racoon', 'cat', 'dog'];
+let domain = ['.es', '.com', '.net', '.org', '.net'];
+
+//Bucle e impresión de array 
+
+for (let i = 0; i < pronoun.length; i++){
+    for (let x = 0; x < adj.length; x++) {
+        for (let y = 0; y < noun.length; y++){
+            for (let z = 0; z < domain.length; z++){
+                console.log(pronoun[i]+adj[x]+noun[y]+domain[z]);
+            }
+        }
+    }
+}
